753b333749d0b570d616e0d2fa30ea8ac4702587
Arnesh Rajendran <arnesha4261@gmail.com>
2026-03-31 22:40:12 +0530

    Error -1 fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL, // now http://localhost:5000
     withCredentials: true,
+    headers: {
+    'ngrok-skip-browser-warning': 'true' // This bypasses the ngrok landing page
+  }
 });
 
 export default api;
